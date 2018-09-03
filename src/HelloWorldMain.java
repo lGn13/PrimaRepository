0754a341e8012ea34cd309282c9cdd3308fbe45a
Pietro Tiana <pietro.tiana.1994@gmail.com>
2018-09-03 11:45:17 +0200

    edited main with new string

--- src/HelloWorldMain.java
+++ src/HelloWorldMain.java
@@ -3,6 +3,9 @@ public class HelloWorldMain {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Ariciao eh!");
+		
 		System.out.println("aggiunto da me");
+		
+		System.out.println("il main è diverso");
 	}
 }
